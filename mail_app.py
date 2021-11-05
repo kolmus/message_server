6079648e186b1012116d92c9f68d2f60887f9744
kolmus <lukasz.kolmus@gmail.com>
2021-11-05 11:49:16 +0100

    made clcrypto and mail _app got lost_of_messages

---
+++ mail_app.py
@@ -0,0 +1,29 @@
+import argparse
+import psycopg2
+from models import Message, User
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--username", help="username")
+parser.add_argument("-p", "--password", help="password")
+parser.add_argument("-t", "--to", help="username you want to send message")
+parser.add_argument("-s", "--send", help="content of your message")
+parser.add_argument("-l", "--list", help="list all my messages")
+
+args = parser.parse_args()
+
+def list_of_messages(username, cursor):
+    message_list = []
+    for row in Message.load_all_messages(cursor):
+        if row.username == username:
+            from_username = User.load_user_by_id(cursor, row.from_id)
+            message_list.append(f'From: {from_username}\nDate: {row.creation_date}\n\n{row.text}\n\n')
+    for message in message_list:
+        print(message + (30 * '-'))
+
+
+
+if __name__ == '__mail__':
+    try:
+        pass
+    except psycopg2.OperationalError as error:
+        print("Connection Error: " + error)
--- models.py
+++ models.py
@@ -1,85 +1,5 @@
-import hashlib
+from clcrypto import hash_password
 import random
-
-def generate_salt():
-    """
-    Generates a 16-character random salt.
-
-    :rtype: str
-    :return: str with generated salt
-    """
-    salt = ""
-    for i in range(0, 16):
-
-        # get a random element from the iterable
-        salt += random.choice('abcdefghijklmnopqrstuvwxyz')
-    return salt
-
-def hash_password(password, salt=None):
-    """
-    Hashes the password with salt as an optional parameter.
-
-    If salt is not provided, generates random salt.
-    If salt is less than 16 chars, fills the string to 16 chars.
-    If salt is longer than 16 chars, cuts salt to 16 chars.
-
-    :param str password: password to hash
-    :param str salt: salt to hash, default None
-
-    :rtype: str
-    :return: hashed password
-    """
-
-    # generate salt if not provided
-    if salt is None:
-        salt = generate_salt()
-
-    # fill to 16 chars if too short
-    if len(salt) < 16:
-        salt += ("a" * (16 - len(salt)))
-
-    # cut to 16 if too long
-    if len(salt) > 16:
-        salt = salt[:16]
-
-    # use sha256 algorithm to generate haintegersh
-    t_sha = hashlib.sha256()
-
-    # we have to encode salt & password to utf-8, this is required by the
-    # hashlib library.
-    t_sha.update(salt.encode('utf-8') + password.encode('utf-8'))
-
-    # return salt & hash joined
-    return salt + t_sha.hexdigest()
-
-def check_password(pass_to_check, hashed):
-    """
-    Checks the password.
-    The function does the following:
-        - gets the salt + hash joined,
-        - extracts salt and hash,
-        - hashes `pass_to_check` with extracted salt,
-        - compares `hashed` with hashed `pass_to_check`.
-        - returns True if password is correct, or False. :)
-
-    :param str pass_to_check: not hashed password
-    :param str hashed: hashed password
-
-    :rtype: bool
-    :return: True if password is correct, False elsewhere
-    """
-
-    # extract salt
-    salt = hashed[:16]
-
-    # extract hash to compare with
-    hash_to_check = hashed[16:]
-
-    # hash password with extracted salt
-    new_hash = hash_password(pass_to_check, salt)
-
-    # compare hashes. If equal, return True
-    return new_hash[16:] == hash_to_check
 
 class User:
     def __init__(self, username="", password="", salt=""):
@@ -183,6 +103,16 @@ class Message:
         return self._creation_date
     
     def save_to_db(self, cursor):
+        """Saves changes to database. 
+        If username eist - update informations.
+        If usernema is new - create  position and return id.
+
+        Args:
+            cursor ([type]): [description]
+
+        Returns:
+            Bool: True if went good
+        """        
         if self._id == -1:
             sql = """INSERT INTO Message(from_id, to_id, creation_date, text) VALUES ('%s', '%s') RETURNING id, creation_date;"""
             values = (self.from_id, self.to_id, self.text,)
@@ -198,6 +128,14 @@ class Message:
 
     @staticmethod
     def load_all_messages(cursor):
+        """Loading all messages id database
+
+        Args:
+            cursor ([type]): [description]
+
+        Returns:
+            List of objects: Each row in database is one object.
+        """        
         sql = "SELECT id, from_id, to_id, text, creation_date FROM Messages;"
         messages = []
         cursor.execute(sql)
